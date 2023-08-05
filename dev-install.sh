71ba2cd6fe7df8572235a9795835300a709861fc
santi-nue <68065589+santi-nue@users.noreply.github.com>
2023-08-05 19:22:59 +0200

    Update dev-install.sh

--- dev-install.sh
+++ dev-install.sh
@@ -1,38 +1,20 @@
 #!/usr/bin/env bash
 
-# For a clean conda environment, do:
-
-# conda create -c conda-forge -n ipywidgets notebook=4.4.1
-# source activate ipywidgets
-# pip install jupyterlab==0.16.2
-# ipython kernel install --name ipywidgets --display-name "ipywidgets" --sys-prefix
-# git clone https://github.com/ipython/ipywidgets.git
-# cd ipywidgets
-# ./dev-install.sh
-
-echo -n "Checking npm... "
-npm -v
-if [ $? -ne 0 ]; then
-    echo "'npm -v' failed, therefore npm is not installed.  In order to perform a
-    developer install of ipywidgets you must have both npm and pip installed on your
-    machine! See http://blog.npmjs.org/post/85484771375/how-to-install-npm for
-    installation instructions."
-    exit 1
-fi
+# For a clean conda environment please read docs/source/dev_install.md
 
 echo -n "Checking pip... "
 pip --version
 if [ $? -ne 0 ]; then
     echo "'pip --version' failed, therefore pip is not installed. In order to perform
-    a developer install of ipywidgets you must have both pip and npm installed on
+    a developer install of ipywidgets you must have pip installed on
     your machine! See https://packaging.python.org/installing/ for installation instructions."
     exit 1
 fi
 
-echo -n "Checking jupyter lab... "
+echo -n "Checking JupyterLab (assuming JupyterLab >=4)... "
 jupyter lab --version 2>/dev/null
 if [ $? -ne 0 ]; then
-    echo "no, skipping installation of jupyterlab_widgets"
+    echo "no, skipping installation of widgets for jupyterlab"
     skip_jupyter_lab=yes
 fi
 
@@ -42,34 +24,25 @@ set -e
 
 nbExtFlags="--sys-prefix $1"
 
-cd jupyter-js-widgets
-npm install
-cd ..
+# echo -n "Installing and building all yarn packages"
+# jlpm
+# jlpm build
 
-cd widgetsnbextension
-npm install
-npm run update
-pip install -v -e .
+echo -n "widgetsnbextension"
+pip install -v -e ./python/widgetsnbextension
 if [[ "$OSTYPE" == "msys" ]]; then
-    jupyter nbextension install --overwrite --py $nbExtFlags widgetsnbextension
+    jupyter nbextension install --overwrite --py $nbExtFlags widgetsnbextension || true
 else
-    jupyter nbextension install --overwrite --py --symlink $nbExtFlags widgetsnbextension
+    jupyter nbextension install --overwrite --py --symlink $nbExtFlags widgetsnbextension || true
 fi
-jupyter nbextension enable --py $nbExtFlags widgetsnbextension
-cd ..
+jupyter nbextension enable --py $nbExtFlags widgetsnbextension || true
+
+echo -n "ipywidgets"
+pip install -v -e "./python/ipywidgets[test]"
 
 if test "$skip_jupyter_lab" != yes; then
-    cd jupyterlab_widgets
-    npm install
-    npm run update
-    pip install -v -e .
-    if [[ "$OSTYPE" == "msys" ]]; then
-        jupyter labextension install --overwrite --py $nbExtFlags jupyterlab_widgets
-    else
-        jupyter labextension install --overwrite --py --symlink $nbExtFlags jupyterlab_widgets
-    fi
-    jupyter labextension enable --py $nbExtFlags jupyterlab_widgets
-    cd ..
+    echo -n "jupyterlab_ipywidgets"
+    pip install jupyter_packaging
+    pip install -ve ./python/jupyterlab_widgets
+    jupyter labextension develop ./python/jupyterlab_widgets --overwrite
 fi
-
-pip install -v -e .
